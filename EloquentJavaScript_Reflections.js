9fb8184cc0303b19f906d1a93dfc782954a3801a
jorivanc <jorivanc@hotmail.com>
2019-09-17 10:43:21 -0700

    Add file EloquentJavaScript_Reflections.js Update: Values, Types and Operators

---
+++ EloquentJavaScript_Reflections.js
@@ -0,0 +1,37 @@
+/*
+
+- Javascript uses 64 bits to store a single number value. negative numbers use a bit to store the sign (-)
+- CALCULATIONS WITH FRACTIONS ARE NOT PRECISE
+- To prevent unexpected TYPE CONVERSION use a === b or a !== b, if both sides are the same type use == or !=
+- To set DEFAULT VALUES use ||. console.log(name || "default"); if name is null or ""(empty string) then the default will be used
+
+Types
+  Numbers: can be represented by : 705, -705, 7.05, 7.05e2, Infinity, -Infinity, NAN (Not A Number, results when 0/0).
+  Booleans: true, false ( 0 == false, 1 == true, '' == false)
+  Strings: 'a string', "another string", `one more string`
+    scaping characters with \: \n: new line, \t: tab, \\: backslash
+    template literals: `${100/50} 100/50` will evaluate what is inside output: 2 100/50
+  Empty Values: null, undefined (null == undefined)
+    Testing for real Values: (null == Value) will be true only if value==null or value==undefined otherwise will be false
+
+Operators:
+  + addition with number and concatenation with strings
+  - substraction
+  * multiplication
+  / division
+  % module
+  typeof VAR will specified the type of the variable VAR
+  condition ? true : false; conditional operator.
+
+  Comparison:
+    >, <, >=, <=, ==, !=, ===, !==
+
+  Logical Operators:
+    &&, ||, ! (logical negation)
+
+------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+*/
